3a017e70bf891aca6e885181a1f005b634dff64b
Railimaa <ferreiralimarai0@gmail.com>
2023-09-10 21:35:24 -0300

    add responsive layout in dashboard

--- src/pages/Dashboard/components/Accounts/index.tsx
+++ src/pages/Dashboard/components/Accounts/index.tsx
@@ -1,9 +1,47 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons';
+import { EyeIcon } from '../../../../assets/icons/EyeIcon';
 import { Container } from './style';
 
 export function Accounts() {
     return (
         <Container>
-            <p>accounts</p>
+            <span className='title'>Saldo total</span>
+
+            <div className="saldo-btn">
+                <strong>R$ 1000,00</strong>
+                <button type='button'>
+                    <EyeIcon open />
+                </button>
+            </div>
+
+            <div className="my-acc">
+
+                <div className='title-acc'>
+                    <strong>Minhas contas</strong>
+
+                    <div className="actions">
+                        <button>
+                            <span>
+                                <ChevronLeftIcon />
+                            </span>
+                        </button>
+
+                        <button>
+                            <span>
+                                <ChevronRightIcon />
+                            </span>
+                        </button>
+                    </div>
+
+                </div>
+
+                <div>
+                  contas...
+                </div>
+
+            </div>
+
+
         </Container>
     );
 }
--- src/pages/Dashboard/components/Accounts/style.ts
+++ src/pages/Dashboard/components/Accounts/style.ts
@@ -1,6 +1,8 @@
 import { styled } from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
   background: #087F5B;
   border-radius: 16px;
   width: 100%;
@@ -13,4 +15,87 @@ export const Container = styled.div`
   @media(min-width: 768px) {
     padding: 40px;
   }
+
+    .title {
+      display: block;
+      letter-spacing: -0.5px;
+      color: #fff;
+    }
+
+    .saldo-btn {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+
+      button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 32px;
+        width: 32px;
+        background: transparent;
+        border: none;
+      }
+
+      strong {
+        color: #fff;
+        font-size: 32px;
+        letter-spacing: -1px;
+      }
+    }
+
+
+    .my-acc {
+      display: flex;
+      justify-content: end;
+      flex-direction: column;
+      flex: 1;
+
+      .title-acc {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        strong {
+          letter-spacing: -1px;
+          color: #fff;
+          font-size: 18px;
+        }
+
+        .actions {
+
+          button {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            padding-left: 10px;
+            padding-right: 14px;
+            border: none;
+            border-radius: 9999px;
+            color: #fff;
+            font-weight: bold;
+            background: transparent;
+            transition: background 0.3s, opacity 0.3s;
+
+            &:hover {
+              background: rgba(0, 0, 0, 0.1);
+              opacity: 1;
+            }
+
+            &:disabled {
+              opacity: 0.4;
+              background: none;
+            }
+
+            span {
+              width: 24px;
+              height: 24px;
+            }
+          }
+        }
+
+
+      }
+
+    }
+
 `;
