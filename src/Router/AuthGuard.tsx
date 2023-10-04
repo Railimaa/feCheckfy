da6ea258b64e9934950a130c73aac972ee3763cd
Railimaa <ferreiralimarai0@gmail.com>
2023-10-04 16:55:27 -0300

    add component Card in component Transaction in Dashboard

--- src/Router/AuthGuard.tsx
+++ src/Router/AuthGuard.tsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../contexts/useAuth';
 
 interface AuthGuardProps {
-  isPrivate: boolean
+  isPrivate: boolean;
 }
 
 export function AuthGuard({ isPrivate }: AuthGuardProps) {
